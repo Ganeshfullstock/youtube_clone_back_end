a7d283a50074078c6b84a3e821889785f701c3c0
Ganeshfullstock <ganesh999siva@gamil.com>
2023-06-26 17:42:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+//////////////////////////////* routes *\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+app.use("/api/user", require("./routes/auth"));
+app.use("/api", require("./routes/video"));
+app.use("/api/", require("./routes/user"));
+
+// DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true,  },)
+
+ .then(() => console.log('connected successfully'))
+.catch((err => {console.error(err);}));
+
+// PORT
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}`);
+});
