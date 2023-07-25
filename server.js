b3265670ed3c3e60a30c5877573f1781b52b210b
Ganesh <123050867+Ganeshfullstock@users.noreply.github.com>
2023-07-25 12:36:13 +0530

    first commit

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(cors());
+   // origin:"http://localhost:3000"
+    origin:"https://react-clone-295320.web.app"
 app.use(morgan("dev"));
 
 //////////////////////////////* routes *\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
